1a3fb5ada8f0cd2bda29e6b2b6d2e0c49cee0d75
Stefan Eng <eng_steff@hotmail.com>
2014-11-21 07:48:02 +0100

    Create parent using structure base class.

--- structure.py
+++ structure.py
@@ -9,14 +9,21 @@ class NestingDoll:
     def add(self, child):
         self.children.append(child)
 
-    def contents(self,indent=None):
-        contents = []
+    def content(self,indent=None):
+        content = []
         if not indent:
             indent = self.indent
-        contents.append(self.header)
+        content.append(self.header)
         for child in self.children:
-            contents.extend(child.contents(indent=indent+indent)
+            content.extend(child.content(indent=indent+indent))
         content.append(self.footer)
-        indent_level = (indent/2) - 1
-        adjusted_content = [" "*indent_level+item for item in content]
+        indent_level = ((indent/2) - 1) * 2
+        adjusted_content = [(" "*indent_level)+item for item in content]
         return adjusted_content
+
+class Parent(NestingDoll):
+
+    def __init__(self, indent): 
+        header='<parent xmlns="http://www.w3.org/2000/svg" version="1.1">'
+        footer='</parent>'
+        NestingDoll.__init__(self, header=header, footer=footer, indent=indent)
