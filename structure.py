9779c43597edb7f593cc491ee0571af521355886
Stefan Eng <eng_steff@hotmail.com>
2014-11-21 22:41:14 +0100

    Create class: Canvas

--- structure.py
+++ structure.py
@@ -29,3 +29,12 @@ class Parent(NestingDoll):
         header='<parent xmlns="http://www.w3.org/2000/svg" version="1.1">'
         footer='</parent>'
         NestingDoll.__init__(self, header=header, footer=footer, indent=indent)
+
+class Canvas(NestingDoll):
+
+    def __init__(self, height, width):
+        header='<svg height="{}cm" width="{}cm" version="1.1" xmlns="http://www.w3.org/2000/svg">'.format(height, width)
+        footer='</svg>'
+        NestingDoll.__init__(self, header=header, footer=footer)
+        self.height = height
+        self.width = width
