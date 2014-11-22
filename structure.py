db912a84aa2651707997f25d90058d37df4001f1
Stefan Eng <eng_steff@hotmail.com>
2014-11-22 17:26:56 +0100

    Loadable in firefox.

--- structure.py
+++ structure.py
@@ -11,15 +11,19 @@ class Parent(NestingDoll):
 class Canvas(NestingDoll):
 
     def __init__(self, width, height):
-        #xml_header = '<?xml version="1.0" standalone="no"?>'
-        #doctype_start = '!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"'
-        #doctype_address = '"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"'
-        #doctype_header = "<{} {}>".format(doctype_start, doctype_address)
-        svg_header = '<svg width="{}cm" height="{}cm">'.format(width, height)
-        #header_list = [xml_header, doctype_header, svg_header]
-        #header = "\n".join(header_list)
+        xml_header = '<?xml version="1.0" standalone="no"?>'
+        doctype_start = '!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"'
+        doctype_address = '"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"'
+        doctype_header = "<{} {}>".format(doctype_start, doctype_address)
+        svg_size_info = 'width="{}cm" height="{}cm"'.format(width, height)
+        svg_style = 'xmlns="http://www.w3.org/2000/svg"'
+        svg_version = 'version="1.1"'
+        svg_header = "<svg {} {} {}>".format(svg_size_info, svg_style,
+                                              svg_version)
+        header_list = [xml_header, doctype_header, svg_header]
+        header = "\n".join(header_list)
         footer='</svg>'
-        NestingDoll.__init__(self, header=svg_header, footer=footer)
+        NestingDoll.__init__(self, header=header, footer=footer)
         self.height = height
         self.width = width
         self.center = NestingDoll.Center(width/2.0, height/2.0)
