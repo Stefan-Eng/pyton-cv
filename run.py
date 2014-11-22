4e0416f5e691ab412412b2db8d1b186201787c1e
Stefan Eng <eng_steff@hotmail.com>
2014-11-22 11:30:46 +0100

    Keep run.py updated.

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
-from structure import Parent
+from structure import Parent,Canvas
+from objects import Rectangle
 
 def main():
 
     parent = Parent(2)
-    other_parent = Parent(2)
-    parent.add(other_parent)
-    print parent.content()
+    canvas = Canvas(15,15)
+    parent.add(canvas)
+    centerx, centery = canvas.center
+    rectangle = Rectangle(centerx,centery,width=5,height=5)
+    canvas.add(rectangle)
+    print '\n'.join(parent.content())
 
 if __name__ == "__main__":
     main()
