5c055a7d9273f7876e8457a602abd51ae2675e80
Stefan Eng <eng_steff@hotmail.com>
2015-01-10 21:54:38 +0100

    Extract text-blocks from input.

--- run.py
+++ run.py
@@ -2,6 +2,35 @@ from structure import Parent,Canvas
 from objects import Rectangle, Line, Text, Defs, Style
 from glyph_data import get_glyph_data
 
+input_text = [line.strip() for line in open("input.txt").readlines()]
+#input_text = [line.strip() for line in open("massive_input.txt").readlines()]
+
+def parse(text):
+    text_blocks = find_textblocks(text)
+    print text_blocks
+
+def find_textblocks(text):
+    all_blocks = []
+    current_block = []
+    previous_was_break = False
+    for line in text:
+        if line == "":
+            if previous_was_break:
+                all_blocks.append(current_block)
+                previous_was_break = False
+                current_block = []
+            elif not previous_was_break:
+                previous_was_break = True
+                continue
+        else:
+            previous_was_break = False
+            if line.endswith('//'):
+                line = line.replace('//','')
+                previous_was_break = True
+            current_block.append(line)
+    if current_block:
+        all_blocks.append(current_block)
+    return all_blocks
 
 def main():
 
@@ -20,8 +49,8 @@ def main():
 
     canvas.add(defs)
 
-
-    print '\n'.join(canvas.content())
+#    print '\n'.join(canvas.content())
+    parse(input_text)
 
 if __name__ == "__main__":
     main()
