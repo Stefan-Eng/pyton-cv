cf7b6e408b5d87611491f0efd691e791789d2d8b
Stefan Eng <eng_steff@hotmail.com>
2015-01-11 07:16:07 +0100

    Grab and parse non-line specific commands, ignore comments.

--- run.py
+++ run.py
@@ -6,14 +6,22 @@ input_text = [line.strip() for line in open("input.txt").readlines()]
 #input_text = [line.strip() for line in open("massive_input.txt").readlines()]
 
 def parse(text):
-    text_blocks = find_textblocks(text)
-    print text_blocks
+    commands, text_blocks = find_textblocks(text)
+
+    print text_blocks, commands
 
 def find_textblocks(text):
     all_blocks = []
     current_block = []
+    commands = []
     previous_was_break = False
     for line in text:
+        if line.startswith('#'):
+            continue
+        if line.startswith('/'):
+            if append_command(line):
+                commands.append(line)
+            continue
         if line == "":
             if previous_was_break:
                 all_blocks.append(current_block)
@@ -30,7 +38,13 @@ def find_textblocks(text):
             current_block.append(line)
     if current_block:
         all_blocks.append(current_block)
-    return all_blocks
+    return commands, all_blocks
+
+def append_command(command):
+    line_dependent_commands = []
+    if command in line_dependent_commands:
+        return False
+    return True
 
 def main():
 
