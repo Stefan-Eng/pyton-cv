5caf14127d18d51673cd39c549493fda749a2b8d
Stefan Eng <eng_steff@hotmail.com>
2015-01-11 16:33:36 +0100

    Support and map unicode characters.

--- glyph_data.py
+++ glyph_data.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import struct
 import json
 
@@ -193,7 +195,9 @@ def get_alphabet(glyph_dict):
                  ('at','@'), ('one','1'), ('two','2'), ('three','3'),
                  ('four','4'),('five','5'),('six','6'),('seven','7'),
                  ('eight','8'),('nine','9'),('zero','0'),('slash','/'),
-                 ('quotesingle',"'")]
+                 ('quotesingle',"'"),('O','Ö'),('o','ö'),('A','Ä'),
+                 ('a','ä'),('A','Å'),('a','å'),('comma',','),
+                 ('exclam','!')]
 
     for letter in 'abcdefghijklmnopqrstuvwxyz':
         alphabet[letter] = glyph_dict[letter]
